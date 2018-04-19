83e186c1ca21768d2331cba7d877f72ba6c8f3d4
alex <alex.erhardt.es@gmail.com>
2018-04-19 17:58:27 +0200

    add first draft form and tweak footer

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('home', { title: `${webname} - Home` });
 });
 
+router.get('/signup', function(req, res,next) {
+  res.render('signup', { title: `${webname} - Signup`});
+});
+
 module.exports = router;
